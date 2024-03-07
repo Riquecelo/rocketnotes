dd3a8d6ff7e4a7347de00ff0408b08ce3d5ff33c
Marcelo Santos <marcelo.has@hotmail.com>
2024-03-07 20:06:03 -0300

    iniciando com styled components

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,11 @@
+import {Container} from './style'
+
+export function Details (){
+  return(
+    <Container>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ea maiores, nulla, aut quasi ad, fuga eveniet soluta consequuntur quisquam laudantium. Sit, ea. Sequi optio repudiandae amet fuga ad autem.</p>
+
+    </Container>
+  )
+}
