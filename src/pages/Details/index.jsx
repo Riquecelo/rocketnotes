b0d098128351de8cf97b8b451e933e99bd9c9909
Marcelo Santos <marcelo.has@hotmail.com>
2024-03-07 22:24:06 -0300

    trabalhando com propriedades

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,5 @@
-import {Container} from './style'
+import { Container } from './style'
+import { Button } from '../../components/Button'
 
 export function Details (){
   return(
@@ -6,6 +7,9 @@ export function Details (){
     <h1>Hello World</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore ea maiores, nulla, aut quasi ad, fuga eveniet soluta consequuntur quisquam laudantium. Sit, ea. Sequi optio repudiandae amet fuga ad autem.</p>
 
+    <Button title="Logar" loading/>
+    <Button title="Sair"/>
+    <Button title="Enviar"/>
     </Container>
   )
 }
