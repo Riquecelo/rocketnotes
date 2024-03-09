4dfeedf1ab290d2a840c7386cdfa5d63d57b89b8
Marcelo Santos <marcelo.has@hotmail.com>
2024-03-09 11:18:50 -0300

    construção do componente Tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,6 +2,7 @@ import { Container, Links } from './style'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
 
 export function Details (){
   return(
@@ -17,7 +18,11 @@ export function Details (){
             <a href="#">Link 2</a>
           </li>
         </Links>
+      </Section>
 
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="node"/>
       </Section>
 
       {/*<Button title="Logar" loading/>*/}
