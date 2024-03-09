194c02e9c48a3e97001c011082af0c92677203c0
Marcelo Santos <marcelo.has@hotmail.com>
2024-03-09 20:22:58 -0300

    construção do menu lateral com correções

--- src/components/ButtonText/index.jsx
+++ src/components/ButtonText/index.jsx
@@ -1,9 +1,10 @@
 import { Conatiner } from "./styles";
 
-export function ButtonText({title, ...rest}){
+export function ButtonText({title, isActive = false, ...rest}){
     return(
         <Conatiner 
             type="button"
+            $isactive={isActive.toString()}
             {...rest}
         >
             {title}
