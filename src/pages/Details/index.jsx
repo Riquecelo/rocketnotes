0a83a6692584114b08de4806d8c56f317603e5ca
Marcelo Santos <marcelo.has@hotmail.com>
2024-03-09 12:02:48 -0300

    fializando página Details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from './style'
+import { Container, Links, Content } from './style'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
@@ -10,27 +10,34 @@ export function Details (){
     <Container>
       <Header/>
 
-      <ButtonText title="Excluir nota"/>
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota"/>
 
-      <Section title="Links úteis">
-        <Links>
-          <li>
-            <a href="#">Link 1</a>
-          </li>
-          <li>
-            <a href="#">Link 2</a>
-          </li>
-        </Links>
-      </Section>
+          <h1>Introdução ao React</h1>
 
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="node"/>
-      </Section>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil obcaecati tempore tenetur incidunt assumenda? Doloribus quidem rerum itaque, commodi ipsa facilis impedit deserunt iste sapiente magnam dolorem, odio iure voluptate.</p>
 
-      {/*<Button title="Logar" loading/>*/}
-      <Button title="Voltar"/>
-    
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">Link 1</a>
+              </li>
+              <li>
+                <a href="#">Link 2</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node"/>
+          </Section>
+
+          {/*<Button title="Logar" loading/>*/}
+          <Button title="Voltar"/>
+        </Content>
+      </main>
     </Container>
   )
 }
