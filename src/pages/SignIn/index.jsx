30e1e403a8b89933f88faeb31b3e02d92c41316d
Marcelo Santos <marcelo.has@hotmail.com>
2024-03-10 15:25:02 -0300

    construção interface login

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,38 @@
-import { Container } from './styles'
+import { Container, Form, Background } from './styles';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
 
 export function SignIn(){
     return(
         <Container>
+            <Form>
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
+                <h2>Faça seu login</h2>
+
+                <Input
+                    placerolder="Email"
+                    type="text"
+                    icon={FiMail}
+                />
+
+                <Input 
+                    placerolder="Senha"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Button title="Entrar" />
+
+            <a href="#">
+                Criar conta
+            </a>
+
+            </Form>
+
+            <Background />
         </Container>
     )
 }
