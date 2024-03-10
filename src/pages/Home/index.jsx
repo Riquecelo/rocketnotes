12c204de176c38f84efc68f00c8cca9b4973f379
Marcelo Santos <marcelo.has@hotmail.com>
2024-03-09 23:54:45 -0300

    montagem de Notas na Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,6 +5,8 @@ import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 import { Header } from '../../components/Header';
 import { ButtonText } from '../../components/ButtonText';
 import { Input } from '../../components/Input';
+import { Section } from '../../components/Section';
+import { Note } from '../../components/Note';
 
 export function Home(){
     return(
@@ -26,7 +28,15 @@ export function Home(){
             </Search>
 
             <Content>
-
+                <Section title="Minha notas">
+                    <Note data={{
+                        title: 'React',
+                        tags: [
+                            {id: '1', name: 'react'},
+                            {id: '2', name: 'node'}
+                        ]
+                    }}/>
+                </Section>
             </Content>
 
             <NewNote>
