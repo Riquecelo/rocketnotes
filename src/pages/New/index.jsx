cb4fbbcb77feff1c47ae75988b9dec8e81b2db5e
Marcelo Santos <marcelo.has@hotmail.com>
2024-03-10 22:52:00 -0300

    contrução da página New e componentes

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,24 @@
+import { Header} from '../../components/Header'
+import { Container, Form } from "./styles";
+import { Input } from '../../components/Input';
+import { Textarea } from '../../components/Textarea';
+
+export function New(){
+    return(
+        <Container>
+            <Header/>
+
+            <main>
+                <Form>
+                    <header>
+                        <h1>Criar nota</h1>
+                        <a href="/">voltar</a>
+                    </header>
+
+                    <Input placeholder="Título"/>
+                    <Textarea placeholder="Observações"/>
+                </Form>
+            </main>
+        </Container>
+    )
+}
